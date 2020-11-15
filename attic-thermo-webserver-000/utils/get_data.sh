c334f2ee5cb20e6fec07329ba6b3e620e486f073
Jaggzh <jaggz.h@gmail.com>
2020-11-15 10:02:44 -0800

    cronjob linux-side log script enhanced

--- attic-thermo-webserver-000/utils/get_data.sh
+++ attic-thermo-webserver-000/utils/get_data.sh
@@ -4,6 +4,10 @@
 # env.sh in the same dir as this script should contain TEMP_HOST,
 #  or you should have it otherwise loaded as an env var
 
+# env.sh, in this script's dir, should contain:
+# TEMP_HOST=ip_of_host
+# STORE_DIR=/path/to/dir
+
 ##################################################################
 # This is all just to get the script's directory, to load env.sh,
 #  ... to get $TEMP_HOST and whatnot
@@ -16,10 +20,10 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-SETTINGS="$DIR/env.sh"
 
 ################################
 # And here we load our settings
+SETTINGS="$DIR/env.sh"
 if [[ ! -e "$SETTINGS" ]]; then
 	echo "WARNING: Missing $DIR/env.sh" >&2
 	echo "We need \$TEMP_HOST. We might fail." >&2
@@ -28,5 +32,9 @@ else
 fi
 
 epoch_secs=$(date +'%s')
+mkdir -p "$STORE_DIR"
+data_file="$STORE_DIR/$(date +'%Y-%m-%d__%H_%M_%S.txt')"
 
-wget -O - "http://$TEMP_HOST/f.txt?t=$epoch_secs"
+echo "Writing to: $data_file" >&2
+wget -O "$data_file" "http://$TEMP_HOST/f.txt?t=$epoch_secs"
+
